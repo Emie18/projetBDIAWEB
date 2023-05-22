eac3322fe8c44bf80b2fc246cade1cf99b2195a3
LeMoalGauthier <gauthier.le-moal@isen-ouest.yncrea.fr>
2023-05-22 14:40:32 +0200

    Suppression des lignes non-valides

--- bigdata/local/commandeG.R
+++ bigdata/local/commandeG.R
@@ -1,7 +1,7 @@
 ##1 Suppression des lignes non-valides
 
 #Importation et visualisation du fichier csv
-Accidents <- read.csv("/ISEN/CIR-3/BigData/Projet/local/stat_acc_V3.csv", sep=";")
+Accidents <- read.csv("/ISEN/CIR-3/BigData/Projet/projetBDIAWEB/bigdata/stat_acc_V3.csv", sep=";")
 View(Accidents)
 summary(Accidents)
 
@@ -14,7 +14,11 @@ summary(Accidents_no_NA)
 #Exemple 1: Ligne 3683 -> longitude > 90
 #Exemple 2: Ligne 3684 -> latitude > 90
 
+#On remarque aussi que certains accidents possèdent un nombre de place NULL :
+#Exemple 3: Ligne 52127 -> place = NULL
+
 #Supression des lignes contenant des valeurs absurdes en suivant la condition suivante :
-Condition <- Accidents_no_NA$longitude < -90 | Accidents_no_NA$longitude > 90 | Accidents_no_NA$latitude < -90 | Accidents_no_NA$latitude > 90
+Condition <- Accidents_no_NA$longitude < -90 | Accidents_no_NA$longitude > 90 | Accidents_no_NA$latitude < -90 | Accidents_no_NA$latitude > 90 | Accidents_no_NA$place == 'NULL'
 Accidents_filtre <- subset(Accidents_no_NA, !Condition)
 summary(Accidents_filtre)
+View(Accidents_filtre)
