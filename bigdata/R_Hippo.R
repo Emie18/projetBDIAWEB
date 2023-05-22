ca6fcb26a198edeeb81e7543678ec9e6b8994487
hippo-Cir <66952675+hippo-Cir@users.noreply.github.com>
2023-05-22 11:57:22 +0200

    Update R_Hippo.R
    Fonction

--- bigdata/R_Hippo.R
+++ bigdata/R_Hippo.R
@@ -1,15 +1,23 @@
-# Spécifier le chemin du fichier CSV
+convertir_formats <- function(chemin_fichier) {
+  # Lire le fichier CSV
+  donnees <- read.csv(chemin_fichier, header = TRUE, sep=";")
+  
+  # Convertir les variables numériques en format numérique
+  variables_numeriques <- c("age", "place", "an_nais", "id_code_insee", "id_usa")
+  donnees[variables_numeriques] <- lapply(donnees[variables_numeriques], as.numeric)
+  
+  # Convertir les variables de date en format date
+  variables_dates <- c("date")
+  donnees[variables_dates] <- lapply(donnees[variables_dates], as.Date)
+  
+  # Retourner les données avec les formats mis à jour
+  return(donnees)
+}
+
+# Utilisation de la fonction
 chemin_fichier <- "C:/Users/33784/Desktop/stat_acc_V3.csv"
-
-# Lire le fichier CSV
-donnees <- read.csv(chemin_fichier, header = TRUE, sep=";")
-
-variables_numeriques <- c("age", "place", "an_nais","id_code_insee","id_usa")
-donnees[variables_numeriques] <- lapply(donnees[variables_numeriques], as.numeric)
-
-# Convertir les variables de date en format date
-variables_dates <- c("date")
-donnees[variables_dates] <- lapply(donnees[variables_dates], as.Date)
+donnees_converties <- convertir_formats(chemin_fichier)
 
 # Afficher les données avec les formats mis à jour
-print(donnees)
+print(donnees_converties)
+
